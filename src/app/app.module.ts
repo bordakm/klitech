5d5c09a1fc7d0101724fce82443637e82fb84dbb
bordakm <mate@webtar.hu>
2020-05-27 07:38:47 +0200

    matbutton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import {MatButtonModule} from '@angular/material/button'; 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
